d6728b99883b9e963a8551d85dcaaf0f308c7704
Meg Galvez <meg@oakmaine.com>
2019-05-16 22:32:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match(array)
     new_array = []
     array.each do |item|
-      if item.split("").sort == @word.sort  
+      if item.split("").sort == @word.to_a.sort  
         new_array << item 
       else
         new_array
